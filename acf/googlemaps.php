7595780d064f362d84ee73cabb564ce0bffd28e8
Miguel Garrido <miguel@neuekommunikation.de>
2022-05-12 13:20:33 +0200

    googlemaps.php key changed

--- acf/googlemaps.php
+++ acf/googlemaps.php
@@ -1,5 +1,5 @@
 <?php
 
 add_action('acf/init', function(){
-  acf_update_setting('google_api_key', 'AIzaSyD44WY58GWuv-typiO-uTBfmCentLEdW3s');
+  acf_update_setting('google_api_key', 'ADD YOUR KEY HERE');
 });
